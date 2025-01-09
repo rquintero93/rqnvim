6bdb0f58390445fc242f7c813ae796893703aa8c
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-09 19:35:39 +0100

    adds hover documentation

---
+++ lua/custom/plugins/lspsaga.lua
@@ -0,0 +1,43 @@
+return {
+
+  {
+    'nvimdev/lspsaga.nvim',
+    event = 'LspAttach',
+    config = function()
+      require('lspsaga').setup {
+        callhierarchy = {
+          enable = false,
+        },
+        definition = {
+          enable = false,
+        },
+        diagnostic = {
+          enable = false,
+        },
+        finder = {
+          enable = false,
+        },
+        term_toggle = {
+          enable = false,
+        },
+        implement = {
+          enable = false,
+        },
+        lightbulb = {
+          enable = true,
+          virtual_text = false,
+        },
+        rename = {
+          enable = false,
+        },
+        -- ui = {
+        --   code_action = '',
+        -- },
+      }
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
+}
