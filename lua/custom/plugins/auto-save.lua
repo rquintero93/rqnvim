332e647f81a9c65aae6e430033580399fedf0dbe
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-09 19:36:03 +0100

    fixes autosave bug with sql

--- lua/custom/plugins/auto-save.lua
+++ lua/custom/plugins/auto-save.lua
@@ -1,8 +1,9 @@
 return {
   'okuuva/auto-save.nvim',
   version = '^1.0.0', -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-  cmd = 'ASToggle', -- optional for lazy loading on command
-  -- event = { 'InsertLeave', 'TextChanged' }, -- optional for lazy loading on trigger events
+  ft = { 'python', 'lua', 'vim', 'bash', 'markdown', 'toml', 'yaml' }, -- optional, leave empty to apply on all filetypes
+  -- cmd = 'ASToggle', -- optional for lazy loading on command
+  -- event = { 'InsertLeave' }, -- optional for lazy loading on trigger events
   opts = {
     -- your config goes here
     -- or just leave it empty :)
