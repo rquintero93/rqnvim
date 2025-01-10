c4f60042b49ccc76f7b0149fdbb190b490af3e95
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-10 15:53:52 +0100

    adds new db options for qol

--- lua/custom/plugins/vim-dadbod-ui.lua
+++ lua/custom/plugins/vim-dadbod-ui.lua
@@ -15,5 +15,10 @@ return {
     vim.g.db_ui_use_nerd_fonts = 1
     -- vim.g.db_ui_default_query = 'SELECT * FROM "{table}"'
     vim.g.db_ui_save_location = '~/Documents/Global Alumni/queries/'
+    vim.g.db_ui_auto_execute_table_helpers = 1
+    vim.g.db_ui_show_database_icon = true
+    vim.g.db_ui_use_nerd_fonts = true
+    vim.g.db_ui_use_nvim_notify = false
+    vim.g.db_ui_execute_on_save = false
   end,
 }
