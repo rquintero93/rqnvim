f4c66a37b686f88e8da3843eb428e25a60bc9311
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-10 12:31:17 +0100

    improves increment options

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -152,6 +152,33 @@ vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<cr>', opts)
 -- Neogen annotation keymap
 
 vim.api.nvim_set_keymap('n', '<Leader>cn', ":lua require('neogen').generate()<CR>", { desc = '[c]ode a[n]otation' })
+
+-- dial increment keymaps
+vim.keymap.set('n', '<C-a>', function()
+  require('dial.map').manipulate('increment', 'normal')
+end)
+vim.keymap.set('n', '<C-x>', function()
+  require('dial.map').manipulate('decrement', 'normal')
+end)
+vim.keymap.set('n', 'g<C-a>', function()
+  require('dial.map').manipulate('increment', 'gnormal')
+end)
+vim.keymap.set('n', 'g<C-x>', function()
+  require('dial.map').manipulate('decrement', 'gnormal')
+end)
+vim.keymap.set('v', '<C-a>', function()
+  require('dial.map').manipulate('increment', 'visual')
+end)
+vim.keymap.set('v', '<C-x>', function()
+  require('dial.map').manipulate('decrement', 'visual')
+end)
+vim.keymap.set('v', 'g<C-a>', function()
+  require('dial.map').manipulate('increment', 'gvisual')
+end)
+vim.keymap.set('v', 'g<C-x>', function()
+  require('dial.map').manipulate('decrement', 'gvisual')
+end)
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
