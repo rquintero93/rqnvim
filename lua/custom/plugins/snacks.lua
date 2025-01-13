2ac591bd249565a643a248ff90349cc1de606abf
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-13 12:18:30 +0100

    fixes lazygit commands

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -18,6 +18,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     -- bigfile = { enabled = true },
+    lazygit = { enabled = false },
     animate = {
       enabled = true,
       speed = 100,
