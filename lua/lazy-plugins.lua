d6cee74acd09b5264c85fbdd7cc08d1f016d74a8
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-15 14:00:27 +0100

    minor refactor removing uneeded files

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -16,18 +16,16 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  -- require 'kickstart/plugins/tokyonight',
-
   require 'kickstart/plugins/todo-comments',
 
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
 
-  -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+
   require 'kickstart.plugins.autopairs',
+
   require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
