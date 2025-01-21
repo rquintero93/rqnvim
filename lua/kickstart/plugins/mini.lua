995f3893f9b2211bed41440f6abbe7b5ecddc2b2
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-21 12:06:12 +0100

    minor refactor of mini

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -3,63 +3,10 @@ return {
     'echasnovski/mini.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.operators').setup {
-        {
-          -- Each entry configures one operator.
-          -- `prefix` defines keys mapped during `setup()`: in Normal mode
-          -- to operate on textobject and line, in Visual - on selection.
-
-          -- Evaluate text and replace with output
-          evaluate = {
-            prefix = 'g=',
-
-            -- Function which does the evaluation
-            func = nil,
-          },
-
-          -- Exchange text regions
-          exchange = {
-            prefix = 'gx',
-
-            -- Whether to reindent new text to match previous indent
-            reindent_linewise = true,
-          },
-
-          -- Multiply (duplicate) text
-          multiply = {
-            prefix = 'gm',
-
-            -- Function which can modify text before multiplying
-            func = nil,
-          },
-
-          -- Replace text with register
-          replace = {
-            prefix = 'gr',
-
-            -- Whether to reindent new text to match previous indent
-            reindent_linewise = true,
-          },
-
-          -- Sort text
-          sort = {
-            prefix = 'gs',
-
-            -- Function which does the sort
-            func = nil,
-          },
-        },
-      }
+      require('mini.operators').setup {}
       require('mini.bracketed').setup {}
       require('mini.bufremove').setup {}
-      -- require('mini.files').setup {}
       require('mini.icons').setup {}
       require('mini.indentscope').setup { symbol = '|' }
       require('mini.move').setup {}
