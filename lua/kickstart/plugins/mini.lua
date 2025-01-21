0b33dd09e95f2cbbc1b5b3a1e1f0be8100d8025d
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-21 10:55:35 +0100

    replaces surround and comment with mini versions

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -10,12 +10,59 @@ return {
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
+      require('mini.operators').setup {
+        {
+          -- Each entry configures one operator.
+          -- `prefix` defines keys mapped during `setup()`: in Normal mode
+          -- to operate on textobject and line, in Visual - on selection.
 
+          -- Evaluate text and replace with output
+          evaluate = {
+            prefix = 'g=',
+
+            -- Function which does the evaluation
+            func = nil,
+          },
+
+          -- Exchange text regions
+          exchange = {
+            prefix = 'gx',
+
+            -- Whether to reindent new text to match previous indent
+            reindent_linewise = true,
+          },
+
+          -- Multiply (duplicate) text
+          multiply = {
+            prefix = 'gm',
+
+            -- Function which can modify text before multiplying
+            func = nil,
+          },
+
+          -- Replace text with register
+          replace = {
+            prefix = 'gr',
+
+            -- Whether to reindent new text to match previous indent
+            reindent_linewise = true,
+          },
+
+          -- Sort text
+          sort = {
+            prefix = 'gs',
+
+            -- Function which does the sort
+            func = nil,
+          },
+        },
+      }
       require('mini.icons').setup {}
       require('mini.indentscope').setup { symbol = '|' }
       require('mini.bufremove').setup {}
       require('mini.bracketed').setup {}
       require('mini.move').setup {}
+      require('mini.surround').setup {}
     end,
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
