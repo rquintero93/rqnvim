030fca6db4590868e11e34973308000f588176f9
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-23 21:12:54 +0100

    improves lazy loading for colorscheme

--- lua/custom/plugins/nightfox.lua
+++ lua/custom/plugins/nightfox.lua
@@ -1,6 +1,7 @@
 return {
   'EdenEast/nightfox.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+  lazy = true,
+  priority = 2000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
