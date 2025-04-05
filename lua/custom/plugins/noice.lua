0aeacaf707dea0d93ba0aab883cc9409a673f936
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-05 19:44:35 +0200

    gets rid of useless notify

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -23,6 +23,10 @@ return {
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
         },
+        hover = {
+          enabled = true,
+          silent = true, -- set to true to not show a message if hover is not available
+        },
       },
       -- you can enable a preset for easier configuration
       presets = {
