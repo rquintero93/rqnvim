030dcb70cc9b2d8bd766d483d4a43b3cace45f1c
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-07 19:18:18 +0200

    keymap for floating term close added

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -96,7 +96,7 @@ vim.api.nvim_set_keymap('n', '<leader>vd', '<cmd>lua Open_in_visidata()<CR>', { 
 -- Toggleterm keymaps
 function _G.set_terminal_keymaps()
   --local opts = { noremap = true }
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', '<cmd>ToggleTerm<cr>', opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', '<cmd>ToggleTerm<cr>', opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
