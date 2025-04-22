0072e5f4e5882d46fc597e2d39db40f149809957
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-22 12:24:00 +0200

    fuck stickybuf

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -229,16 +229,6 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
   end,
 })
 
-vim.api.nvim_create_autocmd('BufEnter', {
-  desc = 'Pin the buffer to any window that is fixed width or height',
-  callback = function(args)
-    local stickybuf = require 'stickybuf'
-    if not stickybuf.is_pinned() and (vim.wo.winfixwidth or vim.wo.winfixheight) then
-      stickybuf.pin()
-    end
-  end,
-})
-
 -- adds filetype to http files
 vim.filetype.add {
   extension = {
