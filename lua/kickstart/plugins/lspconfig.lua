33654d3fefaf4b99ee9aa7c7dfba89d0831791e1
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-26 14:34:54 +0200

    transparency and some other ui updates

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -152,6 +152,7 @@ return {
       vim.diagnostic.config { virtual_text = true }
       -- end
 
+      vim.lsp.handlers['$/progress'] = function() end
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
