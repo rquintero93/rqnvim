d194401829d1d921186ec97e4ff4298b5b479f69
Ricardo Quintero <rquinterog93@gmail.com>
2025-04-28 12:14:56 +0200

    adds lsps for bash and dockerfiles

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -169,71 +169,73 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- sqlls = {
-        --   filetypes = { 'sql', 'bqsql' },
-        --   root_dir = function(_)
-        --     return vim.loop.cwd()
-        --   end,
-        --   settings = {},
-        -- },
-        jsonls = {},
-        -- jedi_language_server = { diagnostics = { enable = false }, hover = { enable = false }, jediSettings = { autoImportModules = { 'numpy', 'pandas' } } },
-        basedpyright = {
-          settings = {
-            basedpyright = {
-              analysis = {
-                useLibraryCodeForTypes = true,
-                autoSearchPath = true,
+      local servers =
+        {
+          -- clangd = {},
+          -- gopls = {},
+          -- pyright = {},
+          -- sqlls = {
+          --   filetypes = { 'sql', 'bqsql' },
+          --   root_dir = function(_)
+          --     return vim.loop.cwd()
+          --   end,
+          --   settings = {},
+          -- },
+          jsonls = {},
+          -- jedi_language_server = { diagnostics = { enable = false }, hover = { enable = false }, jediSettings = { autoImportModules = { 'numpy', 'pandas' } } },
+          basedpyright = {
+            settings = {
+              basedpyright = {
+                analysis = {
+                  useLibraryCodeForTypes = true,
+                  autoSearchPath = true,
+                },
+              },
+            },
+          },
+          html = {},
+          cssls = {},
+          ruff = { settings = {} },
+          taplo = {
+            filetypes = { 'toml' },
+            -- IMPORTANTE: esto es necesario para que taplo LSP funcione en repositorios que no sean git
+            root_dir = require('lspconfig.util').root_pattern('*.toml', '.git'),
+          },
+          yamlls = {},
+          -- rust_analyzer = {},
+          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          --
+          -- Some languages (like typescript) have entire language plugins that can be useful:
+          --    https://github.com/pmizio/typescript-tools.nvim
+          --
+          -- But for many setups, the LSP (`ts_ls`) will work just fine
+          ts_ls = {},
+          --
+          bashls = {},
+          dockerls = {},
+          docker_compose_language_service = {},
+          lua_ls = {
+            -- cmd = { ... },
+            -- filetypes = { ... },
+            -- capabilities = {},
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                diagnostics = { disable = { 'missing-fields' }, globals = { 'vim' } },
               },
             },
           },
         },
-        html = {},
-        cssls = {},
-        ruff = { settings = {} },
-        taplo = {
-          filetypes = { 'toml' },
-          -- IMPORTANTE: esto es necesario para que taplo LSP funcione en repositorios que no sean git
-          root_dir = require('lspconfig.util').root_pattern('*.toml', '.git'),
-        },
-        yamlls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        -- Ensure the servers and tools above are installed
+        --  To check the current status of installed tools and/or manually install
+        --  other tools, you can run
+        --    :Mason
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
-        --
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = { disable = { 'missing-fields' }, globals = { 'vim' } },
-            },
-          },
-        },
-      }
-
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
-      require('mason').setup()
+        --  You can press `g?` for help in this menu.
+        require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
@@ -256,6 +258,9 @@ return {
         'eslint-d',
         'selene',
         'markdownlint',
+        'bashls',
+        'dockerls',
+        'docker-compose-language-service',
         -- 'debugpy',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
